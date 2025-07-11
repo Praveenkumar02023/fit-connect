e504d8422b3cef30c7a6280d6b8c2ac7c8c15e4d
pranav27022006 <ps2315001624@gmail.com>
2025-07-11 23:20:03 +0530

    add feed page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Signin from './components/Signin'
-import Signup from './components/Signup'
-import Home from './components/Home'
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+ import Feed from './components/Feed';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-        
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+
+
