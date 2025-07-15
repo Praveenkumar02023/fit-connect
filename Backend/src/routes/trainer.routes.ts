3d9f4546847ba94e679bf0364bed4101cbbe1f46
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-07-15 17:51:02 +0530

    update trainer update profile endpoint to support image upload and update feed to display avatar image if available else default user image.

--- Backend/src/routes/trainer.routes.ts
+++ Backend/src/routes/trainer.routes.ts
@@ -4,6 +4,7 @@ import { authMiddlware } from "../middlewares/auth.middleware";
 import { getTrainerById, Signin, Signup, updateTrainerProfile } from "../controllers/trainer.controller";
 import { getTrainerSessions } from "../controllers/user.controller";
 import { getAllTrainers } from "../controllers/trainer.controller";
+import upload from "../middlewares/multer.middleware";
 
 export const trainerRouter = Router();
 
@@ -11,7 +12,7 @@ trainerRouter.post('/signup',Signup);
 trainerRouter.post('/signin',Signin);
 
 
-trainerRouter.patch('/update',authMiddlware,updateTrainerProfile)
+trainerRouter.patch('/update',authMiddlware,upload.single("image"),updateTrainerProfile)
 trainerRouter.get('/sessions',authMiddlware,getTrainerSessions);
 
 trainerRouter.get('/:id' , authMiddlware ,getTrainerById);
